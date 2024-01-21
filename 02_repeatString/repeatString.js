33c7ab4e50f0861aab7fbdaac27c3371a43ac460
Rambo12321 <rohankaushik2015@gmail.com>
2024-01-22 02:13:47 +0530

    Completed 1, 2 & 3 ;P

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,30 @@
-const repeatString = function() {
+const repeatString = (a,b) => {
+
+    if(b<0)
+    {
+        return 'ERROR';
+    }
+    else if(b===1)
+    {
+        return a;
+    }
+    else if(b===0)
+    {
+        return '';
+    }
+    else
+    {
+        let str ='';
+        let i=0;
+
+        while(i<b)
+        {
+            str+=a;
+            i++;
+        }
+
+        return str;
+    }
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,21 @@
-const reverseString = function() {
+const reverseString = (a) => {
+
+    if(a=='')
+    {
+        return '';
+    }
+
+    let itr=1;
+    let str = '';
+
+
+    while(itr<=a.length)
+    {
+        str+=a.charAt(a.length-itr);
+        itr++;
+    }
+
+    return str;
 
 };
 
