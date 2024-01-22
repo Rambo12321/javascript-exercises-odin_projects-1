c25a6bf8598b72446c4f9af995701907a6aaec84
Rambo12321 <rohankaushik2015@gmail.com>
2024-01-23 00:24:53 +0530

    5th completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
-const sumAll = function(a,b) 
+const sumAll = function(a,b)   //sums the given numbers
 {
-    if(a<0 || b<0 || typeof(a)==String || typeof(b)==String)
+    if(a<0 || b<0 || check(a) || check(b))
     {
         return 'ERROR';
     }
@@ -26,6 +26,15 @@ const sumAll = function(a,b)
     }
     
 };
+
+function check(a)   //check if Integer value is provided or not
+{
+    if(Number.isInteger(a))
+    {
+        return false;
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
