1ae4088c7cc1bb2735e540b1775a3341459e2813
Rambo12321 <rohankaushik2015@gmail.com>
2024-01-22 23:54:22 +0530

    Exercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+function removeFromArray(a,...args) {
+    let arr = Array.from(args);
 
+    for(let i=0;i<args.length;i++)
+    {
+        let itr = a.indexOf(arr[i]);
+        
+        if(itr===-1)
+        {
+            continue;
+        }
+
+        if (arr[i]===a[itr])
+            {
+                a.splice(itr,1);
+            }
+    }
+
+    console.log(a);
+
+    return a;
+    
 };
 
 // Do not edit below this line
