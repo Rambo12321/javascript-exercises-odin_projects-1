43aad76fb8e5e919babb2aa217f6314d042ef46d
Rambo12321 <rohankaushik2015@gmail.com>
2024-01-23 01:05:52 +0530

    7th Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) 
+{
+  let ans = (a-32)*(5/9);
+
+  if(ans==0)
+  {
+    return 0;  
+  }
+
+  return Number(ans.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(a)
+{
+  let ans = (a*(9/5))+32;
+
+  if(ans==0)
+  {
+    return 0;  
+  }
+
+  return Number(ans.toFixed(1));
 };
 
 // Do not edit below this line
